40c420a440a15bb18fb5451a9831d1ac24f74e79
Priyanshujain2003 <jainpriyanshu2906@gmail.com>
2023-08-12 17:31:19 +0530

    added

---
+++ protfolio/script.js
@@ -0,0 +1,8 @@
+const burger = document.querySelector('.burger');
+
+const navLinks = document.querySelector('.ul');
+
+burger.addEventListener('click', () => {
+  navLinks.classList.toggle('active')
+  
+});
